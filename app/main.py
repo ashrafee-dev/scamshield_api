420d8e5e01d581482f66afbae4aa73ffd364552b
Mahdi Ashrafee <ashrafeem8@gmail.com>
2026-07-16 17:02:47 +0600

    feat: add get_assesment func to reduce redundency

--- app/main.py
+++ app/main.py
@@ -29,51 +29,26 @@ app = FastAPI()
 @app.post("/email")
 def check_name(item: information):
     #check if item.sender in redis
-    response = client.chat.completions.create(
-        model="deepseek-v4-flash",
-        messages=[
-            {"role": "system", "content": f"""Analyze the following email.{item.body}
-
-                                            Return ONLY valid JSON in exactly this format:
-        {{
-                                            "label" "Scam" | "Scam Likely" | "Safe",
-                                            "score": "High" | "Medium" | "Low",
-                                            "certainty": integer
-                                            "reason" : str
-                                            }}
-
-                                            Return ONLY a JSON object.
-                                            Do NOT wrap the JSON in quotes.
-                                            Do NOT escape quotation marks."""}
-        ]
-        #stream=True
-        #reasoning_effort="high",
-        #extra_body={"thinking": {"type": "disabled"}}
-    ) 
-    assesment = None
-    for i in range(5):
-        if response.choices[0].message.content is None:
-                continue
-        response = json.loads(response.choices[0].message.content)
-        print(response)
-        try:
-            assesment = riskAssesment(**response)
-            print(assesment)
-        except ValidationError as e:
-            print(e.errors())
-
-        return assesment
+    return get_assesment(item.body)
 
 @app.post("/audio")
 async def audio_check(file:UploadFile):
     byte = await file.read()
     with open(f"{file.filename}", "wb") as f:
         f.write(byte)
-    transcript = model.transcribe(f"{file.filename}")
+    transcript = model.transcribe(f"{file.filename}")["text"]
+    #filter out sesitive info
+    transcript = "".join (char for char in transcript if char.isalpha() or char.isspace())
+    return get_assesment(transcript)
+    # will use ffmpeg and whisper
+    # whisper will transcribe and filter out clients data, so AI doesnt get their data
+
+
+def get_assesment(transcription: str) -> riskAssesment | None:
     response = client.chat.completions.create(
         model="deepseek-v4-flash",
         messages=[
-            {"role": "system", "content": f"""Analyze the following audio.{transcript}
+            {"role": "system", "content": f"""Analyze the following- {transcription}
 
                                             Return ONLY valid JSON in exactly this format:
         {{
@@ -106,5 +81,3 @@ async def audio_check(file:UploadFile):
             print(e.errors())
 
         return assesment
-    # will use ffmpeg and whisper
-    # whisper will transcribe and filter out clients data, so AI doesnt get their data
