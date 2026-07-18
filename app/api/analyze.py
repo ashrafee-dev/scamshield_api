4ecb9cbbe64ccf0a7cd7f40c0562e4b2333abc53
Mahdi Ashrafee <ashrafeem8@gmail.com>
2026-07-18 15:48:07 +0600

    chore: change temp audio file path

--- app/api/analyze.py
+++ app/api/analyze.py
@@ -14,16 +14,13 @@ def check_name(item: information):
 @router.post("/audio")
 async def audio_check(file:UploadFile):
     byte = await file.read()
-    filename = f"temp_audio/{file.filename}"
+    filename = f"{file.filename}"
     with open(filename, "wb") as f:
         f.write(byte)
     transcript = audio_transcript(filename)
     os.remove(filename)
-    #filter out sesitive info - later I'll use regex to do it properly
     transcript = "".join (char for char in transcript if char.isalpha() or char.isspace())
     return get_assesment(transcript)
-    # will use ffmpeg and whisper
-    # whisper will transcribe and filter out clients data, so AI doesnt get their data
 
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -50,12 +47,11 @@ async def websocket_endpoint(websocket: WebSocket):
             if kind is None or kind.mime not in Allowed:
                 print("Not allowed type")
                 continue
-            filename = f"temp_audio/audio{file_count}.{kind.extension}"
+            filename = f"audio{file_count}.{kind.extension}"
             with open(filename, "wb") as f:
                 f.write(byte)
             transcript = audio_transcript(filename)
             os.remove(filename)
-            #filter out sesitive info - later I'll use regex to do it properly
             transcript = "".join (char for char in transcript if char.isalpha() or char.isspace())
             file_count += 1
             assesment =  get_assesment(transcript)
