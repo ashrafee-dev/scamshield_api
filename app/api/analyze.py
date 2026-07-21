c7dd204c118fed79fd4e003815c1783303864e52
Mahdi Ashrafee <ashrafeem8@gmail.com>
2026-07-21 22:02:10 +0600

    feat(analyze): remove filetype library and related code, add ByteIO

--- app/api/analyze.py
+++ app/api/analyze.py
@@ -1,5 +1,4 @@
-import os
-import filetype
+import io
 from fastapi import APIRouter, UploadFile, WebSocket, WebSocketDisconnect, Request
 from services import rate_limit
 from models.response import riskAssesment
@@ -19,33 +18,13 @@ def email_check(item: information, request: Request)-> riskAssesment | dict | No
 @router.post("/audio")
 async def audio_check(file:UploadFile, request: Request)-> riskAssesment | dict | None:
 
-    Allowed = {
-        "audio/mpeg",
-        "audio/m4a",
-        "audio/mp4",
-        "audio/wav",
-        "audio/x-wav",
-        "audio/webm",
-        "audio/ogg",
-        "audio/flac",
-    }
-
     assert request.client is not None
     if not rate_limit.check_rate_limit(request.client.host):
         return {"error": "Reached you limit, wait 60 seconds before requesting again"}
     byte = await file.read()
-    kind = filetype.guess(byte)
 
-    if kind:
-        print(kind.mime)
-    if kind is None or kind.mime not in Allowed:
-        print("Not allowed type")
-        return
-    filename = f"audio.{kind.extension}"
-    with open(filename, "wb") as f:
-        f.write(byte)
-    transcript = audio_transcript(filename)
-    os.remove(filename)
+    file_stream =  io.BytesIO(byte)
+    transcript = audio_transcript(file_stream)
     return get_assesment(transcript)
 
 @router.websocket("/ws")
@@ -73,17 +52,9 @@ async def websocket_endpoint(websocket: WebSocket)-> riskAssesment | str | None:
             if not rate_limit.check_rate_limit(websocket.client.host):
                 await websocket.send_json({"error": "Reached you limit, wait 60 seconds before requesting again"})
                 continue
-            kind = filetype.guess(byte) 
-            if kind:
-                print(kind.mime)
-            if kind is None or kind.mime not in Allowed:
-                print("Not allowed type")
-                continue
-            filename = f"audio{file_count}.{kind.extension}"
-            with open(filename, "wb") as f:
-                f.write(byte)
-            transcript = audio_transcript(filename)
-            os.remove(filename)
+
+            file_stream =  io.BytesIO(byte)
+            transcript = audio_transcript(file_stream)
             file_count += 1
             assesment =  get_assesment(transcript)
             if assesment is None: 
