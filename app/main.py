b325d548017cf2fa7a8583e58f0a16dcd786a6eb
Mahdi Ashrafee <ashrafeem8@gmail.com>
2026-07-26 06:22:00 +0600

    ci: fix add pylint config in uv dev (#27)
    * ci: fix pylint workflow to use uv and resolve project dependencies
    
    * fix: add pylint config in uv dev
    
    * chore: remove unused imports
    
    * style: fix pylint warnings and tune ruleset

--- app/main.py
+++ app/main.py
@@ -1,10 +1,5 @@
 from fastapi import FastAPI
 from app.api import analyze
 
-    # if item.sender in r.get("numbers"):
-    #     return 
-    #
-
 app = FastAPI()
 app.include_router(analyze.router)
-
